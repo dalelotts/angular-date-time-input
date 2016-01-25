da7b8c378a71e123bcf5f711763d7a477415e1dd
Dale Lotts <dlotts@knightrider.com>
2016-01-24 18:57:38 -0600

    fix(Add missing npm modules): Fix missing modules and incorrect paths that reuslted from dropping bo

--- paths.js
+++ paths.js
@@ -1,10 +1,10 @@
 /* jshint node:true */
 
 var bower = [
-  'bower_components/jquery/dist/jquery.js',
-  'bower_components/moment/moment.js',
-  'bower_components/angular/angular.js',
-  'bower_components/angular-mocks/angular-mocks.js'
+  'node_modules/jquery/dist/jquery.js',
+  'node_modules/moment/moment.js',
+  'node_modules/angular/angular.js',
+  'node_modules/angular-mocks/angular-mocks.js'
 ];
 var bumpFiles = ['package.json', 'bower.json', 'README.md', 'src/js/*.js'];
 var miscFiles = ['GruntFile.js', 'gulpfile.js', 'karma.conf.js', 'paths.js'];
